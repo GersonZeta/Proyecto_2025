2c600765d69a759a737abe2f819200e8d0ed213d
gersonzeta <gezetab@ucvvirtual.edu.pe>
2025-04-05 11:48:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROYECTO_2025',
+  webDir: 'www'
+};
+
+export default config;
